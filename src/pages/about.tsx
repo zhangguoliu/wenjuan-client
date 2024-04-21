4165f8b4e604a7d9de9375aa1d1ee14a067ddbb8
zgl <guoliuzhang@foxmail.com>
2024-04-21 15:52:28 +0800

    体验 Static Generation 预渲染方式

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,10 +1,15 @@
 /*
  * @Author: zgl
- * @Description: TODO 初试 Next.js 新建页面功能（无需 React Router）
+ * @Description: TODO ① 初试 Next.js 新建页面功能（无需 React Router）
+ *                    ② 体验 Static Generation 预渲染方式
  */
 import Head from "next/head";
 
-export default function About() {
+type PropsType = {
+  info: string;
+};
+
+export default function About(props: PropsType) {
   return (
     <>
       <Head>
@@ -15,7 +20,22 @@ export default function About() {
       </Head>
       <main>
         <h1>About page</h1>
+        <p>{props.info}</p>
       </main>
     </>
   );
+}
+
+/**
+ * @description: 此为固定写法，即必须包含 async 和 getStaticProps
+ * @return {*} // return 写法也是固定的，返回的静态属性信息需要放到 props 键值中
+ */
+export async function getStaticProps() {
+  console.log("仅在 build 构建时运行此函数，在线上环境时不会运行");
+  // async function 中可以通过 await 来异步获取数据
+  return {
+    props: {
+      info: "仅在构建时获取的静态属性信息",
+    },
+  };
 }
